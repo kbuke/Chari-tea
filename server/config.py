0839d3ef9b570260f35ec5fa08029b06e1fa4bd3
Kaan Buke <kabuke13@gmail.com>
2024-06-23 19:31:32 +0200

    latest commit

--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 # Standard library imports
-
+import os
 # Remote library imports
 from flask import Flask
 from flask_cors import CORS
@@ -8,9 +8,11 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 
 
 # Local imports
+load_dotenv()
 
 # Instantiate app, set attributes
 app = Flask(
@@ -19,7 +21,7 @@ app = Flask(
     static_folder='../client/build',
     template_folder='../client/build'
 )
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(DATABASE_URI)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 app.secret_key = b'\x9f\x1f\xc2\xc8\xb1\x00\x1aJ\xf6z\xe3np\x15\x91\xd4'
